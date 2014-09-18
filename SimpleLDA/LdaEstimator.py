1f456adad61161141d543c3e36c571eacc74a676
BAmine <amine.benhalloum@gmail.com>
2014-09-18 17:09:51 +0200

    Added EM variational estimator

--- SimpleLDA/LdaEstimator.py
+++ SimpleLDA/LdaEstimator.py
@@ -1,4 +1,5 @@
 from LdaInference import LdaInference
+from LdaModel import LdaModel
 from scipy.special import psi
 import numpy as np
 
@@ -22,12 +23,68 @@ class LdaEstimator(object):
         return likelihood
 
     def write_word_assignements(self,file,doc,phi,model):
-        file.write("{:3d}",doc.length)
+        file.write("{:3d}".format(doc.length))
         for n in xrange(doc.length):
-            file.write("{:4d}:{:2d}",doc.words[n],np.argmax(phi[n]))
+            file.write("{:4d}:{:2d}".format(doc.words[n],np.argmax(phi[n])))
         file.write("\n")
 
-    def save_gamma(self):
+    def save_gamma(self,fileName,gamma,num_docs,num_topics):
+        f=open(fileName)
+        for d in xrange(num_docs):
+            line=" ".join("{5:10f}".format(g) for g in gamma[d])
+            f.write(line)
+            f.write("\n")
+        f.close()
+
+    def run_EM(self,startFileName,directory,corpus,num_topics,startType=None):
+
+        em_converged=10
+        max_iter=10
+        var_gamma=np.zeros(corpus.num_docs)
+        phi=np.zeros(num_topics,max(corpus.docs,key=lambda d: d.length))
+        if startType=="seeded":
+            model=LdaModel(corpus.vocab_size,num_topics,"seeded",corpus)
+        if startType=="random":
+            model=LdaModel(corpus.vocab_size,num_topics,"random")
+        else:
+            model=LdaModel(corpus.vocab_size,num_topics)
+        filename=directory+"/000"
+        #aba: save the lda model to file name
+
+        likelihood=0.0
+        likelihood_old=0.0
+        converged=1
+        likelihood_filename=directory+"/likelihood.dat"
+        likelihood_file=open(likelihood_filename)
+        i=0
+        while(converged <0 or converged > em_converged or (i<=2 and i<=max_iter)):
+            i+=1
+            print " ****** EM iteration {0} ***** \n".format(i)
+            for d in xrange(corpus.num_docs):
+                if d%1000==0:
+                    print "document {0}"%d
+                    likelihood+=self.docEStep(corpus.docs[d],var_gamma[d],phi,model)
+            model.maximum_likelihood(1)
+            converged=(likelihood_old-likelihood)/likelihood_old
+            if converged<0:
+                max_iter=max_iter*2
+            likelihood_old=likelihood
+
+            likelihood_file.write("{10:10f}\t{5:5e} \n".format(likelihood,converged))
+            if i%10==0:
+                my_file=directory+"/{d}".format(i)
+                #save lda model
+                my_file=directory+"/{d}.gamma".format(i)
+                self.save_gamma(my_file,var_gamma,corpus.n_docs,model.num_topics)
+
+        #output model
+
+
+
+
+
+
+
 
 
 
