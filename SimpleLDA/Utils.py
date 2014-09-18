1e1ec8a72f3d9d10c8d339b310e1c43f6ec84854
BAmine <amine.benhalloum@gmail.com>
2014-09-18 17:53:29 +0200

    Added function to optimize for alpha

--- SimpleLDA/Utils.py
+++ SimpleLDA/Utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import log
+from scipy.special import gamma, psi,polygamma
+import scipy.optimize
 
 
 def logSum(loga, logb):
@@ -7,5 +9,27 @@ def logSum(loga, logb):
         return logb + log(1 + np.exp(loga - logb))
     else:
         return loga + log(1 + np.exp(logb - loga))
+
+def alpha_likelihood(a,ss,D,K):
+    return D*(gamma(K*a)-K*gamma(a)) + (a-1)*ss
+
+def d_alpha_likelihood(a,ss,D,K):
+    return D*(K*psi(K*a)-K*psi(a)) + ss
+
+def d2_alpha_likelihood(a,ss,D,K):
+    return D*(K*K*polygamma(1,K*a)-K*polygamma(1,K*a)(a))
+
+
+def opt_alpha(alpha_ss,num_docs,num_topics):
+    def f(a):
+        return alpha_likelihood(a,alpha_ss,num_docs,num_topics)
+
+    def fprime(a):
+        return d_alpha_likelihood(a,alpha_ss,num_docs,num_topics)
+
+    log_alpha_opt=scipy.optimize.fmin_bfgs(f,100,fprime)
+    return np.exp(log_alpha_opt)
+
+
 
 
