89b476e1725fe031e11f35e6b3103bcf42af858f
BAmine <amine.benhalloum@gmail.com>
2014-09-18 17:10:19 +0200

    Added method to save model to file

--- SimpleLDA/LdaModel.py
+++ SimpleLDA/LdaModel.py
@@ -4,15 +4,17 @@ import numpy.random as rnd
 
 
 class LdaModel(object):
-    def __init__(self, vocab_size, n_topics):
+    def __init__(self, vocab_size, n_topics,initType=None,corpus=None):
         self.NUM_INIT = 1
-        self.alpha = 1
         self.log_prob_w = np.zeros((n_topics, vocab_size))
         self.vocab_size = vocab_size
         self.num_topics = n_topics
-        self.class_words = np.zeros((n_topics, vocab_size))
-        self.class_total = np.zeros(n_topics)
-        self.num_docs = 0.0
+        if initType=="seeded":
+            self.corpus_initialize(corpus)
+        if initType=="random":
+            self.random_initialize(vocab_size,n_topics)
+        else:
+            self.zero_initialize(vocab_size,n_topics)
 
     def zero_initialize(self, vocab_size, n_topics):
         self.alpha = 0
@@ -49,6 +51,21 @@ class LdaModel(object):
         if estimate_alpha == 1:
             self.alpha = opt_alpha(self.alpha, self.num_docs, self.num_topics)
             print "new alpha = {0} \n".format(self.alpha)
+
+    def save(self,name):
+        f_name=name+".beta"
+        f=open(f_name)
+        for i in xrange(self.num_topics):
+            f.write(" ".join("{5:10f}\n".format(b) for b in self.log_prob_w[i]))
+        f.close()
+        f_name=name+".other"
+        f=open(f_name)
+        f.write("num topics: {0}\n".format(self.num_topics))
+        f.write("vocab size: {0}\n".format(self.vocab_size))
+        f.write("alpha: {5:10f}\n".format(self.alpha))
+        f.close()
+
+
 
 
 
