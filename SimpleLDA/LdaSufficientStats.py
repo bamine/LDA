8c7d5d4854729e1dc53a5f16e34da90576009245
BAmine <amine.benhalloum@gmail.com>
2014-09-18 17:53:47 +0200

    Added sufficient statistics

---
+++ SimpleLDA/LdaSufficientStats.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class LdaSufficientStats(object):
+    def __init__(self,n_topics,vocab_size,initType=None):
+        self.alpha_ss = 0
+        self.class_words = np.zeros((n_topics, vocab_size))
+        self.class_total = np.zeros(n_topics)
+        self.num_docs = 0.0
